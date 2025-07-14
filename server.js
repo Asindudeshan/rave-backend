95901dab62d7114a429efc828b8b79166d600a7c
Asindu Deshan <130549448+Asindudeshan@users.noreply.github.com>
2025-07-14 22:22:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,20 @@ app.use(
   })
 );
 
+// Always set CORS headers for all responses
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.static("public"));
