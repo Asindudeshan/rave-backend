11f3b9a738414c75549657cce4fef966ceb657f0
Asindu Deshan <130549448+Asindudeshan@users.noreply.github.com>
2025-07-14 22:00:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,12 @@ const db = knex(knexConfig);
 // Middleware
 
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json());
 app.use(express.static("public"));
 
